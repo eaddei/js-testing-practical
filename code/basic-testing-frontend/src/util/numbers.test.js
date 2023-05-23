7f8781b33f568d37ed059b7276faf4defa1f0eab
Ebenezer Addei <ebenezerkaddei@gmail.com>
2023-05-23 16:16:36 -0600

    added a describe block to the test functions

--- code/basic-testing-frontend/src/util/numbers.test.js
+++ code/basic-testing-frontend/src/util/numbers.test.js
@@ -2,15 +2,22 @@
 // import { transformToNumber } from './numbers';
 const {transformToNumber} = require('./numbers');
 
-test('should transform a string number to a number of type number', () => {
-    const input = '1';
-    const result = transformToNumber(input);
-    console.log(result);
-    expect(result).toBe(1);
+describe('should have a function for transforming values to numbers', () => {
+    test('should transform a string number to a number of type number', () => {
+        const input = '1';
+        const result = transformToNumber(input);
+        console.log(result);
+        expect(result).toBe(1);
+    });
+    
+    test('should yield NaN for non-transformable values', () => {
+        const input = 'invalid';
+        const result = transformToNumber(input);
+        expect(result).toBeNaN();
+    
+    });
+
 });
 
-test('should yield NaN for non-transformable values', () => {
-    const input = 'invalid';
-    const result = transformToNumber(input);
-    expect(result).toBeNaN();
-});
+
+
